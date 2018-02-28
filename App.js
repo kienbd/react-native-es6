bb2d56c93d521df8a85aacd9961a710d248a54ed
KienBui <doankien.bui@gmail.com>
2018-02-28 21:37:59 +0200

    remove package-lock

--- App.js
+++ App.js
@@ -6,8 +6,6 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text> Hello Piano  dep trai co gi sai </Text>
       </View>
     )
   }
